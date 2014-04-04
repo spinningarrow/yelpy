03b0f4098c10fc385b27fdf359fb916774505491
Sahil Bajaj <sahil29@gmail.com>
2014-04-04 17:54:16 +0800

    Print results to stdout (as JSON)

--- crawler/yelpSearch.py
+++ crawler/yelpSearch.py
@@ -1,5 +1,7 @@
 from yelpapi import YelpAPI
+import json
 
 yelp_api = YelpAPI('a5oBT4RnnHpt6rtHQWsGYg', '61UpHpxNDVJfpACumIIJNIR2nJ8', '-PlfjeTyjnMSGP67pQbopdxJ-lXE70Bn', 'ewGlo-jkCt0LpNYphzr2SxaEtCY')
 search_results = yelp_api.search_query(location='singapore')
 
+print json.dumps(search_results)
