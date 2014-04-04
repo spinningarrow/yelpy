18508e292f1339e7423b9ff704adb382b488b049
Sahil Bajaj <sahil29@gmail.com>
2014-04-04 16:34:16 +0800

    Fix indentation

--- crawler/yelpSearch.py
+++ crawler/yelpSearch.py
@@ -1,4 +1,5 @@
 from yelpapi import YelpAPI
-	yelp_api = YelpAPI('a5oBT4RnnHpt6rtHQWsGYg', '61UpHpxNDVJfpACumIIJNIR2nJ8', '-PlfjeTyjnMSGP67pQbopdxJ-lXE70Bn', 'ewGlo-jkCt0LpNYphzr2SxaEtCY')
-	search_results = yelp_api.search_query(location='singapore')
+
+yelp_api = YelpAPI('a5oBT4RnnHpt6rtHQWsGYg', '61UpHpxNDVJfpACumIIJNIR2nJ8', '-PlfjeTyjnMSGP67pQbopdxJ-lXE70Bn', 'ewGlo-jkCt0LpNYphzr2SxaEtCY')
+search_results = yelp_api.search_query(location='singapore')
 
