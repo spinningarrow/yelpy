876bbf312f02c0b47033454d933f04d31223e9f4
Sahil Bajaj <sahil29@gmail.com>
2014-04-15 01:47:01 +0800

    Fixed insertion error

--- web/yelpy/yelpy/views.py
+++ web/yelpy/yelpy/views.py
@@ -17,52 +17,55 @@ def index(request):
 #     if request.method == 'POST':
 #         global globvar
 #         globvar = request.POST.get('id', '')
-    
+
 
 @csrf_exempt
 def profile(request):
     if request.method == 'POST':
         form = AllForm(data=request.POST)
         # print form.errors
-        # if form.is_valid():
-            
+        if form.is_valid():
+            form.save()
+
+        else:
+
             # chinese = form.cleaned_data['chinese']
             # indian = form.cleaned_data['indian']
             # vietnamese = form.cleaned_data['vietnamese']
             # thai = form.cleaned_data['thai']
             # western = form.cleaned_data['western']
-        userId = request.POST.get('id', '')
-        obj = All.objects.get(id=userId)
-        
-        obj.chinese = request.POST.get('chinese', '')
-        obj.indian = request.POST.get('indian', '')
-        obj.vietnamese = request.POST.get('vietnamese', '')
-        obj.thai = request.POST.get('thai', '')
-        obj.western= request.POST.get('western', '')
-        obj.fitness = request.POST.get('fitness', '')
-        obj.golf = request.POST.get('golf', '')
-        obj.football = request.POST.get('football', '')
-        obj.swimming = request.POST.get('swimming', '')
-        obj.fishing = request.POST.get('fishing', '')
-        obj.clothes = request.POST.get('clothes', '')
-        obj.accessories = request.POST.get('accessories', '')
-        obj.women = request.POST.get('women', '')
-        obj.children = request.POST.get('children', '')
-        obj.men = request.POST.get('men', '')
-        obj.gifts = request.POST.get('gifts', '')
-        obj.gallery = request.POST.get('gallery', '')
-        obj.cinema = request.POST.get('cinema', '')
-        obj.theater = request.POST.get('theater', '')
-        
-            
-        obj.save()
+            userId = request.POST.get('id', '')
+            obj = All.objects.get(id=userId)
+
+            obj.chinese = request.POST.get('chinese', '')
+            obj.indian = request.POST.get('indian', '')
+            obj.vietnamese = request.POST.get('vietnamese', '')
+            obj.thai = request.POST.get('thai', '')
+            obj.western= request.POST.get('western', '')
+            obj.fitness = request.POST.get('fitness', '')
+            obj.golf = request.POST.get('golf', '')
+            obj.football = request.POST.get('football', '')
+            obj.swimming = request.POST.get('swimming', '')
+            obj.fishing = request.POST.get('fishing', '')
+            obj.clothes = request.POST.get('clothes', '')
+            obj.accessories = request.POST.get('accessories', '')
+            obj.women = request.POST.get('women', '')
+            obj.children = request.POST.get('children', '')
+            obj.men = request.POST.get('men', '')
+            obj.gifts = request.POST.get('gifts', '')
+            obj.gallery = request.POST.get('gallery', '')
+            obj.cinema = request.POST.get('cinema', '')
+            obj.theater = request.POST.get('theater', '')
+
+
+            obj.save()
 
             # return render(request, 'yelpy/profile.html', {
             #   'chinese': chinese, 'indian':indian, 'vietnamese':vietnamese,'thai':thai,'western':western,
             # })
         return HttpResponse(json.dumps(form.data), content_type="application/json")
-            
-    else:        
+
+    else:
         form = AllForm()
 
 
